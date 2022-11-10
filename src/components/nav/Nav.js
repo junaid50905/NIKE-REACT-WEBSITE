5c8f77c0c05779cc5748fa1a399716754fab976b
junaid hossain <junaidhossain50905@gmail.com>
2022-11-11 00:12:19 +0600

    add nav background

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './Nav.css'
 import logo from '../../assets/logo.png'
 import {
@@ -7,19 +7,35 @@ import {
     BsHandbag
 } from "react-icons/bs";
 const Nav = () => {
+  const [navState, setNavState] = useState(false)
+  const onNavSroll = ()=>{
+    if (window.scrollY > 70) {
+      setNavState(true)
+    } else {
+      setNavState(false)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', onNavSroll)
+    return(
+      window.addEventListener('scroll', onNavSroll)
+    )
+  }, [])
+  console.log(navState);
+  
   return (
-    <nav>
-      <div className="logo">
-        <img src={logo} alt="" />
-      </div>
-      <div className="nav-items">
-        <ul>
-            <li><BsSearch className='nav-icons'/></li>
-            <li><BsHeart className='nav-icons'/></li>
-            <li className='cart'><BsHandbag className='nav-icons'/><span>5</span></li>
-        </ul>
-      </div>
-    </nav>
+      <nav onScroll = {onNavSroll} className={`${navState && 'onscroll'}`}>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="nav-items">
+          <ul>
+              <li><BsSearch className='nav-icons'/></li>
+              <li><BsHeart className='nav-icons'/></li>
+              <li className='cart'><BsHandbag className='nav-icons'/><span>5</span></li>
+          </ul>
+        </div>
+      </nav>
   )
 }
 
